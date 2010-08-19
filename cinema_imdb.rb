bacd1d22d4db7b8f40dd0fbb1c846cbd4579e412
Roman Kamyk jr <roman.kamyk@gmail.com>
2010-08-19 21:03:34 +0200

    nicer output

--- cinema_imdb.rb
+++ cinema_imdb.rb
@@ -14,17 +14,32 @@ end
 url = 'http://www.cinema-city.pl/index.php?module=movie&action=repertoire&cid=1081'
 list = read_uri(url)
 titles = list.search("a").select { |a| a['href'] =~ /module=movie&id=/ }.map { |a| {:pl => a.content, :en => a.ancestors("td").at("h4").content} }
-titles.map do |h|
+movies = titles.map do |h|
   search = read_uri URI.encode("http://www.imdb.com/find?s=tt&q=#{h[:en]}")
   page = read_uri(File.join("http://www.imdb.com/", search.search("div#main a").first["href"])) rescue search
-  title = page.at("meta[@name='title']")["content"]
   info = page.search("#tn15content .info")
-  infos = Hash[*info.map { |i| [ (i.at("h5").content.gsub(/:\s*$/, '') rescue nil), (i.at(".info-content").content.gsub(/\s+/, ' ').strip rescue nil) ].compact }.reject { |x| x.length < 2 }.flatten]
-  ap infos
-  rating = page.at(".starbar-meta").content.to_f
-  # h[:genre] = genre
-  h[:rating] = rating
-  h[:imdb_title] = title
-  ap h
-  h
+  infos = Hash[*info.map do |i| 
+    [ 
+      (i.at("h5").content.gsub(/:\s*$/, '').downcase.intern rescue nil), 
+      (i.at(".info-content").content.gsub(/\s+/, ' ').strip.sub(/\s*See more.*$/, '').gsub(/\s*\|\s*/, ', ') rescue nil) 
+    ].compact
+  end.reject { |x| x.length < 2 }.flatten]
+  infos[:genre] = page.search("#tn15content .info").find { |d| d.content =~ /\s*Genre/ }.search("a")[0..-2].map(&:content).join(", ")
+  infos[:rating] = page.at(".starbar-meta").content.to_f
+  infos[:title] = page.at("meta[@name='title']")["content"]
+  infos.merge(h)
+end
+
+def pu(h, k)
+  puts "#{k}: #{h[k]}"
+end
+
+movies.sort_by { |h| -h[:rating] }.each do |m|
+  puts "#{m[:pl]} (#{m[:en]}) [#{m[:title]}]"
+  puts m[:rating].to_s + " " + "*" * m[:rating].to_i
+  pu m, :genre
+  pu m, :tagline
+  pu m, :plot
+  pu m, :director
+  puts "-" * 40
 end
