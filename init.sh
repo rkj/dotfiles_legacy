721a688dc54eaa52966b5071da46c78506b0d9b4
Roman Kamyk <roman.kamyk@gmail.com>
2011-10-12 14:32:37 +0200

    allign

--- init.sh
+++ init.sh
@@ -15,17 +15,17 @@ for i in gitconfig gitignore hgignore irbrc tmux.conf vim vimrc; do
 done;
 
 if [[ ! -e $HOME/.zshrc ]]; then
-  echo '[[ -e $HOME/.profile ]] && source $HOME/.profile' >$HOME/.zshrc
-  echo "[[ -e $root/.profile ]] && source $root/.profile" >$HOME/.zshrc
-  echo "export ZSH=$root/oh-my-zsh" >>$HOME/.zshrc
-  echo 'export ZSH_THEME="rkj-repos"' >>$HOME/.zshrc
-  echo 'source $ZSH/oh-my-zsh.sh' >>$HOME/.zshrc
-  echo "source $root/zshrc" >>$HOME/.zshrc
+  echo '[[ -e $HOME/.profile ]] && source $HOME/.profile' > $HOME/.zshrc
+  echo "[[ -e $root/.profile ]] && source $root/.profile" >> $HOME/.zshrc
+  echo "export ZSH=$root/oh-my-zsh"                       >> $HOME/.zshrc
+  echo 'export ZSH_THEME="rkj-repos"'                     >> $HOME/.zshrc
+  echo 'source $ZSH/oh-my-zsh.sh'                         >> $HOME/.zshrc
+  echo "source $root/zshrc"                               >> $HOME/.zshrc
 fi
 if [[ ! -e $HOME/.hgrc ]]; then
-  echo "echo [ui]" >$HOME/.hgrc
-  echo "username = Please fill me" >>$HOME/.hgrc
-  echo "%include /home/rkj/misc/scripts/hgrc" >>$HOME/.hgrc
+  echo "echo [ui]"                            >$HOME/.hgrc
+  echo "username = Please fill me"            >> $HOME/.hgrc
+  echo "%include /home/rkj/misc/scripts/hgrc" >> $HOME/.hgrc
 fi
 
 
