81357335da95f2589005f37e1b2f83a525391548
Roman Kamyk <roman.kamyk@gmail.com>
2011-10-14 14:55:03 +0200

    fix for Ruby 1.8

--- bin/billmonk_history_to_csv.rb
+++ bin/billmonk_history_to_csv.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+if ARGV.length < 1
+  puts "Usage: #{$0} filename"
+  exit 1
+end
+
 require 'rubygems'
 require 'nokogiri'
 require 'date'
 require RUBY_VERSION >= "1.9" ? 'csv' : 'fastercsv' 
 require 'ap'
 
-if ARGV.length < 1
-  puts "Usage: #{$0} filename"
-  exit 1
+if RUBY_VERSION < "1.9"
+  class Float
+    def round(pos = 0)
+      x = self * (10 ** pos) + 0.5
+      x.to_i.to_f / (10 ** pos)
+    end
+  end
 end
+
 file = ARGV[0]
 html = Nokogiri::HTML(open(file))
 table = html.search("table.standard_orange")
